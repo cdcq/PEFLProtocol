38053f228116d70c3afb573fa6d64685e2edafc6
Kaian Jiang <65334542+oh-jka@users.noreply.github.com>
2022-05-13 00:23:05 +0800

    Run edge in other terminal not in main_pefl

--- tests/main_pefl.py
+++ tests/main_pefl.py
@@ -1,13 +1,16 @@
 import os
 import sys
+sys.path.append(os.path.join(sys.path[0], ".."))
 sys.path.append(os.path.join(sys.path[0], "../pefl_protocol"))
+# print(sys.path)
 
 import random
 import yaml
 import torch
 from time import sleep
 from multiprocessing import Process
-# from torch.multiprocessing import Pool, Process, set_start_method
+# from torch.multiprocessing import Process as gpuProcess
+from torch.multiprocessing import Pool
 
 from pefl_protocol.helpers import yield_accumulated_grads, flatten, de_flatten
 from pefl_protocol.connector import Connector
@@ -22,17 +25,18 @@ from ML_utils.local_update import local_update
 
 
 DIR_OF_AUTH = "cert"
-# KGC_ADDR_PORT = ('127.0.0.1', 8700)
-# CP_ADDR_PORT = ('127.0.0.1', 8701)
-# SP_ADDR_PORT = ('127.0.0.1', 8702)
-KGC_ADDR_PORT = ('127.0.0.1', random.randint(1000, 9999))
-CP_ADDR_PORT = ('127.0.0.1', random.randint(1000, 9999))
-SP_ADDR_PORT = ('127.0.0.1', random.randint(1000, 9999))
+KGC_ADDR_PORT = ('127.0.0.1', 8700)
+CP_ADDR_PORT = ('127.0.0.1', 8701)
+SP_ADDR_PORT = ('127.0.0.1', 8702)
+# KGC_ADDR_PORT = ('127.0.0.1', random.randint(1000, 9999))
+# CP_ADDR_PORT = ('127.0.0.1', random.randint(1000, 9999))
+# SP_ADDR_PORT = ('127.0.0.1', random.randint(1000, 9999))
 TRAINERS_COUNT = 3
 MAX_ROUND = 1000
 DATASET_NAME = "mnist"
 MODEL_NAME = "MLP"
 MODEL_LENGTH = 633226
+DEVICE = torch.device("cuda")
 
 
 def register_users():
@@ -107,70 +111,64 @@ def run_service_provider():
 
 
 def run_edge():
+    print("1111")
     key_generator = Connector(service=KGC_ADDR_PORT,
                               ca_path=os.path.join(DIR_OF_AUTH, "kgc.crt"))
     service_provider = Connector(service=SP_ADDR_PORT,
                                  ca_path=os.path.join(DIR_OF_AUTH, "sp.crt"))
+    print("2222")
     edge = Trainer(
         key_generator=key_generator,
         service_provider=service_provider,
         token_path=os.path.join(DIR_OF_AUTH, "token", "edge.yml"),
         model_length=MODEL_LENGTH,
     )
+    print("333")
     train_dataset = get_train_dataset(dataset="mnist", iid=True)
+    print("444")
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
-    model = get_model(model_name="mlp")
+    print("5555")
+    model = get_model(model_name="mlp", device=DEVICE)
+    print("edge 正在启动")
     for _ in range(MAX_ROUND):
-        grads_list, local_loss = local_update(model=model, dataloader=train_dataloader)
+        grads_list, local_loss = local_update(model=model, dataloader=train_dataloader, device=DEVICE)
         grads_vector = flatten(yield_accumulated_grads(grads_list))
         weights_vector = edge.round_run(grads_vector)
         de_flatten(vector=weights_vector, model=model)
+
 
 if __name__ == "__main__":
     # run_edge()
     # exit(0)
 
-    # torch.multiprocessing.set_start_method("serverfork")
-
+    # torch.multiprocessing.set_start_method('spawn')
     register_users()
-    kgc_process = Process(target=run_kgc, args=())
+    kgc_process = Process(target=run_kgc, args=(), daemon=True)
     kgc_process.start()
     sleep(3)
-    cp_process = Process(target=run_cloud_provider, args=())
+    cp_process = Process(target=run_cloud_provider, args=(), daemon=True)
     cp_process.start()
     sleep(2)
-    sp_process = Process(target=run_service_provider, args=())
+    sp_process = Process(target=run_service_provider, args=(), daemon=True)
     sp_process.start()
     sleep(2)
 
+    exit_flag = input("输入exit以结束：")
+    while exit_flag != "exit":
+        exit_flag = input()
+
+    exit()
+
     # 会报错
     # RuntimeError: Cannot re-initialize CUDA in forked subprocess. To use CUDA with multiprocessing, you must use the 'spawn' start method
-    for i in range(1):
-        t_process = Process(target=run_edge, args=())
-        t_process.start()
-
-    # register_users()
-    # kgc_process = Process(target=run_kgc, args=(), daemon=True)
-    # kgc_process.start()
-    # sleep(3)
-    # cp_process = Process(target=run_cloud_provider, args=(), daemon=True)
-    # cp_process.start()
-    # sleep(2)
-    # sp_process = Process(target=run_service_provider, args=(), daemon=True)
-    # sp_process.start()
-    # sleep(2)
-    #
-    # try:
-    #     set_start_method('spawn')
-    # except RuntimeError:
-    #     pass
-    #
-    # t_process = [0, 0, 0]
-    # for i in range(1):
-    #     t_process[i] = Process(target=run_edge, args=(), daemon=True)
-    #     t_process[i].start()
+    # # torch.multiprocessing.set_start_method('spawn', force=True)
+    # p = Pool(TRAINERS_COUNT)
+    # for i in range(TRAINERS_COUNT):
+    #     p.apply_async(func=run_edge, args=())
     #
     # exit_flag = input("输入exit以结束：")
     # while exit_flag != "exit":
     #     exit_flag = input()
+    # p.close()
+    # p.join()
     # exit()
---
+++ tests/run_edge.py
@@ -0,0 +1,4 @@
+from main_pefl import run_edge
+
+if __name__ == "__main__":
+    run_edge()
